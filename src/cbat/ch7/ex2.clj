2a0b310b09aa10cef2c26c04d69492cdee5d71ee
dan <daniel.picton@gmail.com>
2017-01-30 07:03:11 +0000

    Converted process-triplet to function, made some progress, but mainly wrote notes in comments as what I need to do next.

--- src/cbat/ch7/ex2.clj
+++ src/cbat/ch7/ex2.clj
@@ -3,22 +3,27 @@
 
 (apply require clojure.main/repl-requires)
 
+; NEXT:
+; Convert infix to function, do all processing there, ignore properly implementing process-triplet for now (just use as template).
+
+
+; Currently works if only one triplet (i.e. last-group? is true). Ultimately will be a function called by a parent macro, which calls infix for operators: / * + -
+; Destructuring (I think) needs to move to process-triplet function (apart from last-group?), otherwise recurring through inlist becomes convoluted as I think I'll need a separate output list. Not sure though. I think I may be better making infix a helper function and do all conditionals there so I have finer control of what I'm recurring.
 (defmacro infix [op inlist]
   (let [[i1 i2 & i3] inlist
         last-group? (nil? (second i3))]
     (println i1 " " i2 " " i3 " " last-group?)
     (if last-group?
-      "no more i3 - func call"
-     (if (= i2 op)
-        "match - recur prefix"
-        "no-match - recur unchanged infix")
+      ;"no more i3 - func call"
+      (process-triplet op i1 i2 (first i3))
+      "more i3 - recur infix with process triplet"
      )
     ))
 
 
 
 ; Initial plan was to use process-triplet to determine if i2 from infix matched op and if so, change to prefix. When non-match happens though, this tries to return invalid list; going to put conditional in infix for now and see if I can weave this in later on (saves duplication of condition).
-(defmacro process-triplet
+(defn process-triplet
     "Check if three args are an infix operation, if so return list in prefix notation."
       [test-op operand1 in-op operand2]
       (if (= test-op in-op)
