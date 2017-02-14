67a1b3bdf073b1786c4bacfcd11d291779e99ba3
dan <daniel.picton@gmail.com>
2017-02-14 07:09:38 +0000

    Currently not retrieving correct HTML when using (slurp owlurl), but using the actual url in the slurp works. Need to read up again on promises and futures.

--- src/cbat/ch9/ex1.clj
+++ src/cbat/ch9/ex1.clj
@@ -2,9 +2,26 @@
   (:require [pl.danieljanus.tagsoup :as html])
   (:gen-class))
 
-(def owlurl "http://www.bing.com/search#q=owls")
+(def owlurl "https://www.bing.com/search#q=owls")
+
+(def pretend-webpage "<html><head><h1>Page Header</h1></head><body>Meow</body></html>")
+
+;><li class="b_algo"><h2><a href="http://www.owlpages.com/owls/"
 
 (defn get-search-result [url]
   (let [search-html (future (slurp url))]
     (let [parsed-html (html/parse-string (str @search-html))]
       (println (:html :head parsed-html)))))
+
+(defn get-pretend []
+  (html/parse-string pretend-webpage))
+
+(defn extract-child-node [kw v-in]
+  (let [[k tm & cn] v-in]
+    (if (= kw k)
+      (first cn)
+      (recur kw cn)
+    )))
+
+;[:html {} [:head {}] [:body {} [:h1 {} "Page Header"]] [:body {} "Meow"]]
+
