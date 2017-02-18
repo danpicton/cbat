9084fed71870cf1bfe6ac03c94ed7f15dcc42cfb
dan <daniel.picton@gmail.com>
2017-02-18 06:48:10 +0000

    Code works if I replace the search engines' URLs with a static site, and pass a sub-page to the site as a search term. It appears the search engines' results aren't being returned properly.

--- src/cbat/ch9/ex1.clj
+++ src/cbat/ch9/ex1.clj
@@ -1,33 +1,30 @@
 (ns cbat.ch9.ex1
+  (:import [java.net URL URLEncoder])
   (:require [pl.danieljanus.tagsoup :as html])
   (:gen-class))
 (apply require clojure.main/repl-requires)
 
-(def owlurl "https://www.bing.com/search#q=owls")
 (def user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172")
-
 
 (defn get-google-url [search-term]
   (str "https://www.google.com/search?q=" search-term))
 (defn get-bing-url [search-term]
   (str "https://www.bing.com/search#q=" search-term))
 
+(defn open-connection[url]
+  (doto (.openConnection url)
+    (.setRequestProperty "User-Agent" user-agent)))
+
+(defn get-response [url]
+  (let [conn (open-connection url)
+        sb (StringBuilder.)]
+    (slurp (.getInputStream conn))))
+
 (defn search [search-term]
   (let [web-html (promise)]
     (doseq [url [get-google-url get-bing-url]]
-      (future (if-let [html-string (slurp (url search-term))]
-              (deliver web-html html-string))))
-    @web-html))
-
-
-(defn get-pretend []
-  (html/parse-string pretend-webpage))
-
-(defn extract-child-node [kw v-in]
-  (let [[k tm & cn] v-in]
-    (if (= kw k)
-      (first cn)
-      (recur kw cn)
-    )))
+      (future (if-let [html-string (java.net.URL. (url search-term))]
+              (deliver web-html (get-response html-string)))))
+    (deref web-html 5000 :timeout)))
 
 
