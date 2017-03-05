52c2706ac725da912a3ed6e043e06307b798f762
dan <daniel.picton@gmail.com>
2017-03-05 21:21:34 +0000

    Started Chapter 10, Exercise 3
    Created two refs, more to come!

--- src/cbat/ch10/answers.clj
+++ src/cbat/ch10/answers.clj
@@ -11,7 +11,6 @@
 (def word-map {})
 
 ;; exercise 2
-(def quote-list (atom()))
 
 ;; manually created list for starting things off
 (def word-list '("this" "is" "a" "test" "this" "is" "not" "a" "test" "hello" "hello" "is" "this" "me" "you're" "looking" "for"))
@@ -24,8 +23,7 @@
 (defn get-quote-list 
   "Retrieves parameterised number of quotes from 'http://www.braveclojure.com/random-quote' and returns the word count across all quotes"
   [quote-count]
-  (let [atom-list (promise)
-        quote-list (atom())]
+  (let [quote-list (atom())]
     (doseq [a (range 0 quote-count)]
       (let [curr-quote (future (slurp "http://www.braveclojure.com/random-quote"))]
         (swap! quote-list conj @curr-quote)))
@@ -40,3 +38,22 @@
   "Retrieves number of quotes from 'http://www.braveclojure.com/random-quote' equal to quote-count and returns a map of the counts of each word. "
   [quote-count]
   (get-list-word-count (get-lazy-word-seq (get-quote-list quote-count))))
+
+;; exercise 3
+;; This exercise is a bit vague as it seems like the second character should be giving the first a healing potion, but this isn't clear. Will assume char 2 gives healing potion and char 1 drinks and heals in same transaction.
+(defn create-char
+  "Returns a fresh character with items in inventory."
+  [name & items]
+  {:name name
+   :hp 40
+   :max-hp 40
+   :inventory items}
+  )
+
+(def char-one (ref (create-char "Thandor")))
+(def char-two (ref (create-char "Mork" "Healing potion")))
+
+(defn wound
+  [character pow]
+  ;(dosync (alter character (if (<= 0 (- (:hp character) pow) update-in [:hp] 0 ) (- (:hp character) pow)))))
+  (dosync (alter character (if (<= 0 (- (:hp character) pow)) {:status "alive"} {:status "dead"}))))
